fca88552bf30d2d9b99fb7a6eead8ac8dafd82cc
Ariel Slade <arielslade@Air-de-Ariel.fibertel.com.ar>
2024-10-01 10:43:44 -0300

    add readme

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -1,6 +1,6 @@
 const Ticket = require('../models/Ticket');
 const axios = require('axios');
-const { Op } = require('sequelize');  // Importar los operadores de Sequelize
+const { Op } = require('sequelize');
 require('dotenv').config(); 
 
 // Crear un nuevo ticket
@@ -9,7 +9,7 @@ exports.createTicket = async (req, res) => {
 
   try {
     const getRandomOffset = (max) => {
-      return Math.floor(Math.random() * max);  // Genera un número aleatorio hasta un límite máximo
+      return Math.floor(Math.random() * max);
     };
     
     const gifResponse = await axios.get('https://api.giphy.com/v1/gifs/search', {
@@ -23,13 +23,12 @@ exports.createTicket = async (req, res) => {
     
     const gifUrl = gifResponse.data.data[0].images.original.url;
 
-    // Incluye el status como 'Pending' si no se especifica
     const newTicket = await Ticket.create({ 
       name, 
       description, 
       difficulty, 
       gifUrl, 
-      status: 'Pending'  // Asegúrate de crear el ticket con un estado inicial
+      status: 'Pending'
     });
     res.status(201).json(newTicket);
   } catch (error) {
@@ -41,20 +40,19 @@ exports.createTicket = async (req, res) => {
 exports.getAllTickets = async (req, res) => {
   const { status, difficulty, startDate, endDate } = req.query;
   
-  // Construimos un objeto para las condiciones de búsqueda
   const whereConditions = {};
 
   if (status) {
-    whereConditions.status = status;  // Filtro por estado
+    whereConditions.status = status; 
   }
 
   if (difficulty) {
-    whereConditions.difficulty = difficulty;  // Filtro por dificultad
+    whereConditions.difficulty = difficulty;
   }
 
   if (startDate && endDate) {
     whereConditions.createdAt = {
-      [Op.between]: [new Date(startDate), new Date(endDate)]  // Filtro por fecha de creación
+      [Op.between]: [new Date(startDate), new Date(endDate)]
     };
   }
 
@@ -80,7 +78,7 @@ exports.updateTicket = async (req, res) => {
 
     // Función para generar un offset aleatorio
     const getRandomOffset = (max) => {
-      return Math.floor(Math.random() * max);  // Genera un número aleatorio hasta un límite máximo
+      return Math.floor(Math.random() * max); 
     };
 
     if (difficulty) {
@@ -89,7 +87,7 @@ exports.updateTicket = async (req, res) => {
           api_key: process.env.GIPHY_API_KEY, 
           q: difficulty,
           limit: 1,
-          offset: getRandomOffset(50)  // Genera un offset aleatorio para obtener diferentes GIFs
+          offset: getRandomOffset(50)
         }
       });
       ticket.gifUrl = gifResponse.data.data[0].images.original.url;
@@ -127,10 +125,10 @@ exports.updateTicketStatus = async (req, res) => {
       return res.status(404).json({ error: 'Ticket no encontrado' });
     }
 
-    ticket.status = status;  // Actualiza el estado del ticket
+    ticket.status = status;
     await ticket.save();
 
-    res.json(ticket);  // Retorna el ticket actualizado
+    res.json(ticket);
   } catch (error) {
     res.status(500).json({ error: 'Error al actualizar el estado del ticket' });
   }
